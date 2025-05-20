5e82011fa9902888bc2340bdc83fd754ea6f5bf9
mihajluknazar <mykhana_aeh@students.vizja.pl>
2025-05-20 14:47:33 +0200

    web.API

--- Api/src/router/index.js
+++ Api/src/router/index.js
@@ -1,10 +1,23 @@
+// src/router/index.js
+
 import { createRouter, createWebHistory } from 'vue-router'
-import AllCars from '@/components/AllCars.vue'
 import CarDetails from '@/components/CarDetails.vue'
 
 const routes = [
-  { path: '/', name: 'AllCars', component: AllCars },
-  { path: '/cars/:carId', name: 'CarDetails', component: CarDetails }
+  {
+    path: '/',
+    redirect: () => {
+      // Отримання ID останнього переглянутого авто з localStorage
+      const lastViewedCarId = localStorage.getItem('lastViewedCarId');
+      // Якщо є, редирект на нього, інакше на дефолтне авто
+      return lastViewedCarId ? `/cars/${lastViewedCarId}` : '/cars/67f51cc3e2671ffdd1a93624';
+    }
+  },
+  {
+    path: '/cars/:carId',
+    name: 'CarDetails',
+    component: CarDetails
+  }
 ]
 
 const router = createRouter({
